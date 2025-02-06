32f87a7b4c9c5e7744d97a56c9acb13b7852ffa2
saku3 <saku3@MyComputer>
2025-02-06 21:29:57 +0900

    1_19カリキュラム

---
+++ Java_Curriculum/src/basicClass/Animal.java
@@ -0,0 +1,23 @@
+package basicClass;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Animal {
+
+	public static void main(String[] args) {
+		// Q1：Dogクラスを呼び出して変数に代入し、動物の名前をコンソールに出力してください。
+		Dog dog = new Dog();
+		System.out.println(dog.name);
+		// Q2：Dogクラスを呼び出して変数に代入し、動物の数をコンソールに出力してください。
+		Dog dog2 = new Dog(6);
+		System.out.println(dog2.numbers);
+		// 現在の日時を「yyyy-MM-dd H:m:s」形式で変数に代入して、コンソールに出力してください
+		LocalDateTime nowDate = LocalDateTime.now();
+
+		DateTimeFormatter dtf1 = DateTimeFormatter.ofPattern("yyyy-MM-dd H:m:s");
+		String formatNowDate = dtf1.format(nowDate);
+		System.out.println(formatNowDate);
+
+	}
+};
